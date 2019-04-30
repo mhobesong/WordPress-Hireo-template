9bedb1a3044bce49583f1d9c7f67da235f3c15af
Moses Besong <mosbesong@gmail.com>
2019-04-30 12:14:45 +0100

    add right menu items

--- header.php
+++ header.php
@@ -14,4 +14,14 @@
             <ul class="nav pages">
                 <?php wp_list_pages(['sort_column'=>'menu_order', 'title_li'=>NULL]);?>
             </ul>
+
+			<div class="profile-status">
+				<img src="<?= get_avatar_url(wp_get_current_user()->ID, ['size'=>55]);?>" alt="..." class="img-circle">
+				<span class="online-status online"> </span>
+			</div>
+
+			<div class="notifications">
+				<i class="far fa-bell fa-lg"><span class="badge badge-primary badge-notification">2</span></i>
+				<i class="far fa-envelope fa-lg"><span class="badge badge-primary badge-notification">2</span></i>
+			</div>
         </nav>
